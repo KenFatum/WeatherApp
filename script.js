0dd20d6a1087c188f0eaaa60203405fbd947db30
Furkan Aydin <furkan.aydin@mail.schwarz>
2024-05-09 22:48:05 +0200

    feature:added a script

---
+++ script.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", function () {
+    const nameCity = document.querySelector(".cityname-input");
+    //const selectDays = documents.querySelector(".select-days");
+    const searchBtn = document.querySelector(".search-btn button");
+    const displayCity = document.querySelector(".city-display");
+    const displayCelsius = document.querySelector(".celsius-display");
+    const displayDays = document.querySelector(".days-display");
+    const displayHumidity = document.querySelector(".humidity-display");
+
+    
+})
